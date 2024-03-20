44484fa0b02673b9460b8167fcc1fe01f71dbd39
shalinipriya1011 <70254428+shalinipriya1011@users.noreply.github.com>
2024-03-20 16:31:34 +0530

    added icons

--- constants/index.ts
+++ constants/index.ts
@@ -23,27 +23,27 @@ export const NAV_LINKS = [
   export const FEATURES = [
     {
       title: 'Passion',
-      icon: '/map.svg',
+      icon: '/motivation.png',
       variant: 'green',
       description:
       'We are passionate about what we do and we believe that Real Estate Development can change the landscape of the markets and also to the people’s lives. We’re relentlessly focused on growth opportunities to create increased value for everyone.'
     },
     {
       title: 'Value Creation',
-      icon: '/calendar.svg',
+      icon: '/business-value.png',
       variant: 'green',
       description:
       'We work with a purpose and the purpose is to create value for every stake holder that is associated with us, whether it is a minute detail, a complex design, or a business transaction. We have constantly evolved to the changing markets to promote a positive change.',
     },
     {
       title: 'Commitment',
-      icon: '/tech.svg',
+      icon: '/handshake.png',
       variant: 'green',
       description:
 'We do what we say and believe in doing the right thing. We are honest about what we think and commit only when we have conviction. We celebrate progress not just results. We believe everything we do has to be of the highest possible standard at all times.',    },
     {
       title: 'Sustainability',
-      icon: '/location.svg',
+      icon: '/skyline.png',
       variant: 'orange',
       description:
 'What some call difficult, we love doing it. We go beyond property development to creating meaningful and sustainable communities that are built to last. We care for nature and constantly the best practices to significantly reduce the negative impact on the environment.',    },
